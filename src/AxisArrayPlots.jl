d3f687c03a954dae2557dc6cda298c55d0cd4a3a
Jan Weidner <jw3126@gmail.com>
2017-08-23 17:02:43 +0200

    init

--- src/AxisArrayPlots.jl
+++ src/AxisArrayPlots.jl
@@ -1,5 +1,22 @@
+__precompile__(true)
 module AxisArrayPlots
 
-# package code goes here
+using AxisArrays
+using RecipesBase
+
+@recipe function plot{T}(arr::AxisArray{T, 1})
+    xlabel --> axisnames(arr)[1]
+    xrotation --> -45
+    xs = first(axisvalues(arr))
+    xs, arr.data
+end
+
+@recipe function plot{T}(arr::AxisArray{T, 2})
+    xlbl, ylbl = axisnames(arr)
+    xlabel --> xlbl
+    ylabel --> ylbl
+    xrotation --> -45
+    axisvalues(arr)..., arr.data
+end
 
 end # module
